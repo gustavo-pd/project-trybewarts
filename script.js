e563aa5102c7358ee85fdda8520d89b7c35e29a3
GustavoPD <gudalmoro@gmail.com>
2021-07-27 17:22:08 -0300

    botao submit 18

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const login = document.getElementById('email');
 const password = document.getElementById('password');
 const button = document.getElementById('enter');
+const buttonsbt = document.getElementById('submit-btn');
+const agree = document.getElementById('agreement');
 
 button.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
+  }
+});
+
+buttonsbt.disabled = true;
+agree.addEventListener('click', function() {
+  if (agree.checked === true) {
+    buttonsbt.disabled = false;
+  } else if (agree.checked === false) {
+    buttonsbt.disabled = true;
   }
 });
